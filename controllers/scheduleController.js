2d41ac2af6c11d80ff009e3b951e55d648f1fe5d
Saakshi Jain <saakshi7012@gmail.com>
2021-11-19 21:01:35 +0530

    feat: add schedule controller

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,8 +1,34 @@
 var scheduleCollection = require('../models/schedule');
 
-exports.runn = async (req,res) => {
+async function allFacultiesDoubtsSlotsMapper (data) {
+    const allFacultiesDoubtsSlots = new Map();
+    const daysMapping = new Map([
+    	["Monday",0],
+        ["Tuesday",1],
+        ["Wednesday",2],
+        ["Thursday",3],
+        ["Friday",4],
+    ]);
+    for(let obj in data) {
+        
+    	if (allFacultiesDoubtsSlots.has(data[obj]["name"])) {
+        	var facultySlots = allFacultiesDoubtsSlots.get(data[obj]["name"]);
+            var dayIndex = daysMapping.get(data[obj]["day"]);
+            facultySlots[dayIndex]=data[obj]["availableSlots"];
+            allFacultiesDoubtsSlots.set(data[obj]["name"],facultySlots);
+        } else {
+        	var facultySlots=new Array(5);
+            var dayIndex = daysMapping.get(data[obj]["day"]);
+            facultySlots[dayIndex]=data[obj]["availableSlots"];
+        	allFacultiesDoubtsSlots.set(data[obj]["name"],facultySlots);
+        }
+    }
+    return allFacultiesDoubtsSlots;
+}
+
+exports.allFacultiesDoubtsSlots = async (req,res) => {
     const data = await scheduleCollection.find({});
-    console.log(data);
-    res.render( 'student', { enrollno: req.query.enrollno, time_table: data} );
+    var time_table = await allFacultiesDoubtsSlotsMapper(data);
+    res.render( 'student', { enrollno: req.query.enrollno, time_table: time_table} );
 };
 
--- routes/student.js
+++ routes/student.js
@@ -2,19 +2,6 @@ var express = require('express');
 var router = express.Router();
 var schedule_controller = require('../controllers/scheduleController');
 
-router.get('/',schedule_controller.runn);
+router.get('/',schedule_controller.allFacultiesDoubtsSlots);
 
 module.exports = router;
-
-// mongodb+srv://saakshi:<password>@cluster0.fmyko.mongodb.net/myFirstDatabase?retryWrites=true&w=majority 
-
-
-// const { MongoClient } = require('mongodb');
-// const uri = "mongodb+srv://saakshi:<password>@cluster0.fmyko.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   perform actions on the collection object
-//   client.close();
-// });
-// 
